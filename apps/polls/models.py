845be52daea8ceaab6b35fb85948d2372d9f6908
AakashDayma01 <daymaaakash334@gmail.com>
2026-06-19 17:58:29 +0530

    completed polls app

--- apps/polls/models.py
+++ apps/polls/models.py
@@ -3,6 +3,7 @@ from django.db import models
 import datetime
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
 
 
 # Create your models here.
@@ -14,8 +15,15 @@ class Question(models.Model):
 	def __str__(self):
 		return f"{self.question_text}"
 
+	@admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
+
 	def was_published_recently(self):
-		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+		now = timezone.now()
+		return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 class Choice(models.Model):
 	questions = models.ForeignKey(Question, on_delete = models.CASCADE)
--- config/settings/base.py
+++ config/settings/base.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
@@ -40,9 +41,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.polls',
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -130,3 +133,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR,  'static')]
+
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
+]
